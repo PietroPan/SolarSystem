555de709d64c1d915678c277aaaa96c251805c4b
PietroPan <alexandreferreira200007@gmail.com>
2021-03-14 02:35:28 +0000

    testing gitignore

---
+++ Fase1/Generator/CMakeFiles/generator.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/generator.cpp" "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/CMakeFiles/generator.dir/generator.cpp.o"
+  "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/models/box.cpp" "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/CMakeFiles/generator.dir/models/box.cpp.o"
+  "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/models/cone.cpp" "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/CMakeFiles/generator.dir/models/cone.cpp.o"
+  "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/models/plane.cpp" "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/CMakeFiles/generator.dir/models/plane.cpp.o"
+  "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/models/sphere.cpp" "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/CMakeFiles/generator.dir/models/sphere.cpp.o"
+  "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/utils/model.cpp" "/home/xela/Documents/CG/STUPID/CG/Fase1/Generator/CMakeFiles/generator.dir/utils/model.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
